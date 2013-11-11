25eda3058768e000e5af143ee2217ea58faf09f2
raballand.romain@gmail.com <raballand.romain@gmail.com@11896a7c-cb30-61eb-f20d-3891b1778931>
2013-11-11 13:29:49 +0000

    [maven-release-plugin]  copy for tag meehome-java-user-1.0

---
+++ tags/meehome-java-user-1.0/trunk/meehome-java-user-tools/JtoolBox/JToolBox.bat
@@ -0,0 +1,40 @@
+@echo on
+@echo --------------------------------------------------------------
+@echo -  JTOOLBOX                                                  -
+@echo --------------------------------------------------------------
+@echo off
+
+set CONFIGURATION_FILE=configuration.properties
+
+@REM Applications
+@REM : Directory : lieu ou la console se place à l'ouverture ( je l'ai mis sur mon project par exemple )
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "DIRECTORY" %CONFIGURATION_FILE%') DO set DIRECTORY=%%i
+
+@REM : JAVA_HOME : Permet de switch d'une JDK 1.5 à une autre 1.7 par exemple
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "JAVA_HOME" %CONFIGURATION_FILE%') DO set JAVA_HOME=%%i
+
+@REM : M2_HOME : Permet de switch d'un maven 2.2.1 à un maven 3.0 par exemple
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "M2_HOME" %CONFIGURATION_FILE%') DO set M2_HOME=%%i
+
+@REM : SVN_HOME : Configuration de svn
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "SVN_HOME" %CONFIGURATION_FILE%') DO set SVN_HOME=%%i
+
+@REM : Configuration de maven en terme de proxy, repo distant tout ça ( à venir ) pour le moment ne mettez rien ça gene pas
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "GLOBAL_SETTINGS" %CONFIGURATION_FILE%') DO set GLOBAL_SETTINGS=%%i
+FOR /F "eol=; tokens=2,2 delims==" %%i IN ('findstr /i "USER_SETTINGS" %CONFIGURATION_FILE%') DO set USER_SETTINGS=%%i
+
+@REM Options maven
+set MAVEN_OPTS=-Xms256m %MAVEN_OPTS%
+set MAVEN_OPTS=-Xmx512m %MAVEN_OPTS%
+set MAVEN_OPTS=-XX:PermSize=64m %MAVEN_OPTS%
+set MAVEN_OPTS=-XX:MaxPermSize=512m %MAVEN_OPTS%
+set MAVEN_OPTS=-Duser.language=fr %MAVEN_OPTS%
+set MAVEN_OPTS=-Duser.locale=fr %MAVEN_OPTS%
+@REM set MAVEN_OPTS=-Duser.home=%JTBOX_INST_USER_SETTINGS_DIR% %MAVEN_OPTS%
+set MAVEN_OPTS=-Dorg.apache.maven.global-settings=%GLOBAL_SETTINGS% %MAVEN_OPTS%
+set MAVEN_OPTS=-Dorg.apache.maven.user-settings=%USER_SETTINGS% %MAVEN_OPTS%
+
+set PATH=%JAVA_HOME%\bin;%M2_HOME%\bin;%SVN_HOME%\bin;%PATH%
+cd %DIRECTORY%
+
+cmd.exe
---
+++ tags/meehome-java-user-1.0/trunk/meehome-java-user-webapp/src/main/java/fr/meehome/user/webapp/controller/AccueilController.java
@@ -0,0 +1,65 @@
+package fr.meehome.user.webapp.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import fr.meehome.user.webapp.model.Authentification;
+import fr.meehome.user.webapp.model.User;
+
+@Controller
+@RequestMapping("/accueil")
+public class AccueilController {
+
+    @ModelAttribute("authentification")
+    public Authentification getAuthentification() {
+        return new Authentification();
+    }
+
+    // AFFICHE LA PAGE D'ACCUEIL
+    @RequestMapping(method = RequestMethod.GET)
+    public String accueil(ModelMap model) {
+        model.addAttribute("authentification", new Authentification());
+        return "accueil";
+    }
+
+    // VALIDATION FORMULAIRE DE CONNEXION
+    @RequestMapping(value = "/connexion", method = RequestMethod.POST)
+    public String connexion(@Valid Authentification authentification, BindingResult result, ModelMap model) {
+        if (!result.hasErrors()) {
+            if (!authentification.getLogin().equals("Romain") || !authentification.getPassword().equals("13041983")) {
+                result.addError(new ObjectError("authentification.failed", "Votre login ou votre mot de passe n'est pas valide"));
+            } else {
+                User user = new User();
+                user.setNom("RABALLAND");
+                user.setPrenom("Romain");
+                model.addAttribute("userAuthentificated", user);
+            }
+        }
+        return "accueil";
+    }
+
+    // AFFICHE LA PAGE D'INSCRIPTION
+    @RequestMapping(value = "/inscription", method = RequestMethod.GET)
+    public String inscription(ModelMap model) {
+        model.addAttribute("authentification", new Authentification());
+        model.addAttribute("user", new User());
+        return "inscription";
+    }
+
+    // VALIDATION FORMULAIRE INSCRIPTION
+    @RequestMapping(value = "/inscription", method = RequestMethod.POST)
+    public String connexion(@ModelAttribute("user") @Valid User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "inscription";
+        }
+        return "inscriptionSuccess";
+    }
+
+}
